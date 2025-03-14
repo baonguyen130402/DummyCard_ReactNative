7613726c30867b570b4bf918bb208f0506efd40d
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-15 03:41:19 +0700

    refactor(CardDetail): use renderif made code more clearly

--- components/CardDetail.tsx
+++ components/CardDetail.tsx
@@ -1,5 +1,6 @@
+import RenderIf from "@/helper/renderIf"
 import { useEffect, useState } from "react"
-import { ActivityIndicator, Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { ActivityIndicator, Image, Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
 type CardDetailProps = {
   data: any,
@@ -13,11 +14,52 @@ export default function CardDetail(props: CardDetailProps) {
   const [loading, setLoading] = useState(true)
   const detailData = data[cardId - 1]
 
+  console.log(data)
+
   useEffect(() => {
     setTimeout(() => {
       setLoading(false)
     }, 300)
   }, [])
+
+  const renderDetailInfo = (type: string) => {
+    return (
+      <View style={styles.detailInfo}>
+        {detailData.image && (
+          <Image
+            source={{ uri: detailData.image }}
+            style={styles.detailImage}
+            resizeMode="contain"
+          />
+        )}
+        <View style={{ alignSelf: 'flex-start', marginLeft: 16 }}>
+          <Text style={[styles.detailTitle, { textAlign: 'left' }]}>{detailData.name}</Text>
+          <RenderIf isTrue={type === 'User'}>
+            <Text>Email: {detailData?.email}</Text>
+            <Text>Age: {detailData?.age}</Text>
+            <Text>Gender: {detailData?.gender}</Text>
+            <Text>Phone Number: {detailData?.phoneNumber}</Text>
+            <Text>Address: {detailData?.address?.address}, {detailData?.address?.country}</Text>
+          </RenderIf>
+          <RenderIf isTrue={type === 'Product'}>
+            <Text>Price: {detailData?.price}$</Text>
+            <Text>Discount: {detailData?.discountPercentage}%</Text>
+            <Text>Brand: {detailData?.brand}</Text>
+            <Text>Amount: {detailData?.stock}</Text>
+            <Text>Des: {detailData?.shippingInformation}</Text>
+          </RenderIf>
+          <RenderIf isTrue={type === 'Recipe'}>
+            <Text>Cuisine: {detailData.cuisine}</Text>
+            <Text>Difficulty: {detailData.difficulty}</Text>
+            <Text>Prep Minutes: {detailData.prepTimeMinutes} phút</Text>
+            <Text>Cooking Time: {detailData.cookTimeMinutes} phút</Text>
+            <Text>Serving: {detailData.servings}</Text>
+            <Text>Calo: {detailData.caloriesPerServing} kcal</Text>
+          </RenderIf>
+        </View>
+      </View>
+    )
+  }
 
   return (
     <Modal
@@ -34,23 +76,14 @@ export default function CardDetail(props: CardDetailProps) {
           {/* Render data */}
           {loading ? (
             <ActivityIndicator size="small" color="#0000ff" />
-          ) : detailData ? (
-            <View style={styles.detailInfo}>
-              <Text>Name: {detailData?.name}</Text>
-              <Text>Email: {detailData?.email}</Text>
-              <Text>Age: {detailData?.age}</Text>
-              <Text>Gender: {detailData?.gender}</Text>
-              <Text>Phone Number: {detailData?.phoneNumber}</Text>
-              <Text>Address: {detailData?.address?.address}, {detailData?.address?.country}</Text>
-            </View>
-          ) : null}
+          ) : detailData ? renderDetailInfo(type) : null}
 
           {/* Close button */}
           <TouchableOpacity
             style={styles.closeButton}
             onPress={closeModal}
           >
-            <Text style={styles.closeButtonText}>Đóng</Text>
+            <Text style={styles.closeButtonText}>Close</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -80,6 +113,9 @@ const styles = StyleSheet.create({
   detailInfo: {
     width: '100%',
     marginBottom: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   errorText: {
     color: 'red',
@@ -89,12 +125,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#2196F3',
     padding: 10,
     borderRadius: 5,
-    width: 100,
+    width: '100%',
     alignItems: 'center',
-    marginTop: 16
+    marginTop: 8
   },
   closeButtonText: {
     color: 'white',
     fontWeight: 'bold',
+  },
+
+  detailImage: {
+    width: 250,
+    height: 250,
+    marginBottom: 15,
+  },
+
+  detailTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 5,
   },
 })
