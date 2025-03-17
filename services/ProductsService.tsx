f4fc1a8dee5c8e0d9db47397191b40f74afb1579
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-17 15:54:19 +0700

    perf(ProductServices): save data into AsyncStorage after fetch data successfully

--- services/ProductsService.tsx
+++ services/ProductsService.tsx
@@ -1,9 +1,11 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
 import axios from "axios"
 
 const API_URL = 'https://dummyjson.com/products'
 
 export const getProductsData = async () => {
   const { data: response } = await axios.get(API_URL)
+  await AsyncStorage.setItem('products', JSON.stringify(response.products))
 
   return response ? response.products : []
 }
