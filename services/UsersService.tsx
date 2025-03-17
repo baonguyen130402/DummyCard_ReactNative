99ba6d2a176c14620eb4a083ca8b49f77923c990
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-17 15:53:25 +0700

    perf(UserServices): save data to AsyncStorage after fetch data from API successfully

--- services/UsersService.tsx
+++ services/UsersService.tsx
@@ -1,15 +1,11 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
 import axios from "axios"
 
 const API_URL = 'https://dummyjson.com/users'
 
 export const getUsersData = async () => {
   const { data: response } = await axios.get(API_URL)
+  await AsyncStorage.setItem('users', JSON.stringify(response.users))
 
-  return response ? response.users : []
-}
-
-export const getUserById = async (userId: number) => {
-  const { data: response } = await axios.get(`${API_URL}/${userId}`)
-
-  return response ? response : []
+  return  response ? response.users : []
 }
