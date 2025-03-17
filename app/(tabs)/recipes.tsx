f48c8846a8bfc5455679c8ec30bfd5543193aac0
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-17 15:59:05 +0700

    perf(tabs/recipes): use saved data from AsyncStorage

--- app/(tabs)/recipes.tsx
+++ app/(tabs)/recipes.tsx
@@ -1,17 +1,48 @@
 import CardList from '@/components/CardList';
+import { MESSAGE } from '@/constants/message';
 import { getRecipesData } from '@/services/RecipesService';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 
 export default function RecipesScreen() {
   const [recipes, setRecipes] = useState([{}])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  const d = [] as any[]
 
   useEffect(() => {
     (async () => {
-      const data = await getRecipesData()
-      console.log(data)
-      setRecipes(data)
+      try {
+        setLoading(true)
+
+        const dataStorage = await AsyncStorage.getItem('recipes')
+        const recipesStorage = JSON.parse(dataStorage || '')
+
+        const data = recipesStorage ? recipesStorage : await getRecipesData()
+
+        data.forEach((recipe: any) => {
+          d.push({
+            id: recipe.id,
+            name: recipe.name,
+            cuisine: recipe.cuisine,
+            difficulty: recipe.difficulty,
+            prepTimeMinutes: recipe.prepTimeMinutes,
+            cookTimeMinutes: recipe.cookTimeMinutes,
+            servings: recipe.servings,
+            caloriesPerServing: recipe.caloriesPerServing,
+            image: recipe.image
+          })
+        })
+        setRecipes(d)
+      } catch(err) {
+        setError(MESSAGE.ERROR)
+        console.error(err)
+      } finally {
+        setLoading(false)
+      }
     })()
   }, [])
 
-  return <CardList data={recipes} type='Recipe' />;
+  return <CardList data={recipes} loading={loading} error={error} type='Recipe' />;
 }
