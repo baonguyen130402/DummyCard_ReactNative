3f8e9efee6175dea0e5af45c24f2d866eb12ee5a
Birk Nguyen <baonguyen130402@gmail.com>
2025-03-17 15:54:59 +0700

    perf(RecipesServices): save data into AsyncStorage after fetch data successfully

--- services/RecipesService.tsx
+++ services/RecipesService.tsx
@@ -1,9 +1,11 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
 import axios from "axios"
 
 const API_URL = 'https://dummyjson.com/recipes'
 
 export const getRecipesData = async () => {
   const { data: response } = await axios.get(API_URL)
+  await AsyncStorage.setItem('recipes', JSON.stringify(response.recipes))
 
   return response ? response.recipes : []
 }
